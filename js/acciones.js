90ea664a5d35bb266f12404e964fb485d5647660
neidaNieto <neida.desaupoo@gmail.com>
2016-05-02 11:08:53 -0500

    2subida

--- js/acciones.js
+++ js/acciones.js
@@ -1,13 +1,13 @@
 // JavaScript
 
- $(document).ready(function(e) {
- document.addEventListener("deviceready",function(){
+ $(document).ready(function (e){
+ document.addEventListener ("deviceready",function(){
 	 
  $('#beep').tap(function (){
  navigator.notification.beep (1);	 
 });//tap beep
 
- $('vibrar').tap(function (){
+ $('#vibrar').tap(function (){
   navigator.notification.vibrate (1000);
  });//tap vibrar
  
